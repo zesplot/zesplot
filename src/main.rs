802b6509d24aa6b0100dcd539e032112134b4648
Luuk Hendriks <mail@luukhendriks.eu>
2017-11-09 23:48:08 +0100

    added id: String to Area, implemented more efficient adding of Areas to Rows

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,30 @@
 extern crate svg;
 
 use svg::*;
-use svg::node::*;
-use svg::node::element::*;
 use svg::node::element::{Path, Rectangle};
 use svg::node::element::path::Data;
 
 
-use std::io::{self, BufReader};
+use std::io::{BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 
-//fn ratio(r: Rectangle) -> f64 {
-//    //let (_, _, a) = r;
-//    //r.description.get("w").unwrap()
-//}
-
-#[derive (Copy, Clone)]
+//#[derive (Copy, Clone)]
 struct Area {
     x: f64,
     y: f64,
     w: f64,
     h: f64,
     surface: f64,
+    id: String,
 }
 
 impl Area {
-    fn new(surface: f64, ratio: f64) -> Area {
+    fn new(surface: f64, ratio: f64, id: String) -> Area {
         let w = surface.powf(ratio);
         let h = surface.powf(1.0 - ratio);
         //println!("area::new {} * {}", w, h);
-        Area { x: 0.0, y: 0.0, w, h, surface} 
+        Area { x: 0.0, y: 0.0, w, h, surface, id }
     }
     fn get_ratio(&self) -> f64 {
         if &self.h >= &self.w {
@@ -38,12 +32,6 @@ impl Area {
         } else {
             &self.h / &self.w
         }
-    }
-
-    fn set_height(&mut self, new_h: f64) -> () {
-        //let resize_factor = &self.surface
-        self.w = self.surface / new_h;
-        self.h = new_h;
     }
 }
 
@@ -74,15 +62,27 @@ impl Row {
         Row {x, y, w: area.w, h: area.h, vertical, areas:vec![area]}
     }
 
-    fn test(&mut self, area: Area) -> bool {
-        let mut better = false;
+    fn test(&mut self, area: &Area) -> bool {
         let cur_ratio = self.calc_ratio();
-        &self.push(area);
-        better = self.calc_ratio() >= cur_ratio;
+        let tmp_area = Area {x: 0.0, y: 0.0, w: area.w, h: area.h, surface: area.surface, id: "tmp".to_string()};
+        &self.push(tmp_area);
+        //&self.push(&mut area);
+        let better = self.calc_ratio() >= cur_ratio;
         &self.pop();
         //println!("better? {}", better);
         better
     }
+
+    fn try(&mut self, area: Area) -> Option<Area> {
+        let cur_ratio = self.calc_ratio();
+        &self.push(area);
+        if self.calc_ratio() >= cur_ratio {
+            None
+        } else {
+            self.pop()
+        }
+    }
+
 
     fn reflow(&mut self) -> () {
         //println!("reflow:");
@@ -113,12 +113,24 @@ impl Row {
         }
     }
 
-    fn pop(&mut self) -> () {
-        let _popped = &self.areas.pop();
+    fn pop(&mut self) -> Option<Area> {
+        let popped = self.areas.pop();
         self.reflow();
+        popped
     }
 
     fn push(&mut self, mut area: Area) -> () {
+        if self.vertical {
+            area.x = self.x;
+        } else {
+            area.y = self.y;
+        }
+
+        &self.areas.push(area);
+        self.reflow();
+    }
+
+    fn _push(&mut self, mut area: Area) -> () {
         if self.vertical {
             area.x = self.x;
         } else {
@@ -157,45 +169,68 @@ fn main() {
 
     let mut areas: Vec<Area> = Vec::new();
     for i in inputs {
-        areas.push(Area::new(i * norm_factor, init_ar));
+        areas.push(Area::new(i * norm_factor, init_ar, i.to_string()));
     }
 
 
 
     let mut rows = Vec::new();
-    let (&first_area, remaining_areas) = areas.split_first().unwrap();
+    //let (first_area, remaining_areas) = areas.split_first().unwrap();
+    let remaining_areas = areas.split_off(1); //.pop().unwrap();
+    let first_area = areas.pop().unwrap();
     let (mut new_row_x, mut new_row_y) = (0.0, 0.0);
     rows.push(Row::new(new_row_x, new_row_y, true, first_area));
     let mut i = 0;
 
-    let direction = false; // boolean to work either horizontally or vertically
-    for mut a in remaining_areas {
+    for a in remaining_areas {
 
-        let need_new_row: bool = {
-            let cur_row = rows.last_mut().unwrap();
-            //cur_row.push(a);
-            //false
-            if cur_row.test(*a) {
-                cur_row.push(*a);
-                false
-            } else {
-                true
-            }
-        };
+//        let need_new_row: bool = {
+//            let cur_row = rows.last_mut().unwrap();
+//            //cur_row.push(a);
+//            //false
+//            if cur_row.test(&a) {
+//                //cur_row.push(a);
+//                false
+//            } else {
+//                true
+//            }
+//        };
+//
+//        //let need_new_row = true;
+//
+//        if need_new_row {
+//            let cur_row_w = rows.last().unwrap().w ;
+//            let cur_row_h = rows.last().unwrap().h;
+//            let cur_row_vertical = rows.last().unwrap().vertical;
+//            if cur_row_vertical {
+//                new_row_x += cur_row_w;
+//                //println!("new horizontal row at {},{}", new_row_x, new_row_y);
+//                rows.push(Row::new(new_row_x, new_row_y, false, a));
+//            } else {
+//                new_row_y += cur_row_h;
+//                // create new vertical row
+//                //println!("new vertical row at {},{}", new_row_x, new_row_y);
+//                rows.push(Row::new(new_row_x, new_row_y, true, a));
+//            }
+//            rows.last_mut().unwrap().reflow();
+//        } else {
+//            rows.last_mut().unwrap().push(a);
+//        }
 
-        if need_new_row {
+        if let Some(area) = rows.last_mut().unwrap().try(a) {
+
             let cur_row_w = rows.last().unwrap().w ;
             let cur_row_h = rows.last().unwrap().h;
             let cur_row_vertical = rows.last().unwrap().vertical;
             if cur_row_vertical {
                 new_row_x += cur_row_w;
                 //println!("new horizontal row at {},{}", new_row_x, new_row_y);
-                rows.push(Row::new(new_row_x, new_row_y, false, *a));
+                rows.push(Row::new(new_row_x, new_row_y, false, area));
             } else {
                 new_row_y += cur_row_h;
                 // create new vertical row
                 //println!("new vertical row at {},{}", new_row_x, new_row_y);
-                rows.push(Row::new(new_row_x, new_row_y, true, *a));
+                rows.push(Row::new(new_row_x, new_row_y, true, area));
             }
             rows.last_mut().unwrap().reflow();
         }
@@ -216,14 +251,7 @@ fn main() {
                         "#ff00ff",
                         ];
     let mut i = 0;
-    for mut row in rows {
-        let (direction, color) = {
-            if row.vertical {
-                ("vertical", "red")
-            } else {
-                ("horizontal", "green")
-            }
-        };
+    for row in rows {
         //println!("new row: {}", direction);
         for area in row.areas {
             //println!("{},{} {} * {}", area.x, area.y, area.w, area.h);
@@ -238,6 +266,8 @@ fn main() {
                 .set("stroke", "black")
                 .set("opacity", 0.5)
                 //.set("opacity", (area.x * area.y) / 10000_f64)
+                
+                .set("data-id", area.id)
                 ;
             rects.push(rect);
             i += 1;
